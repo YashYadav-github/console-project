bcaa4ba953462a7abb70e8ea6d3e2ac785e5dbe9
YashYadav-github <74818709+YashYadav-github@users.noreply.github.com>
2021-05-20 08:57:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("My first console project");
+var name1 = "Yash ";
+var name2 = "Yadav";
+var fullName = name1.concat(name2);
+console.log(fullName);
+var num1 = 8;
+var num2 = 4;
+var addition = num1 + num2;
+var subtraction = num1 - num2;
+var multiplacation = num1 * num2;
+var divison = num1 / num2;
+console.log("Addition: "+addition)
+console.log("Subtraction: "+subtraction)
+console.log("Multiplacation: "+multiplacation)
+console.log("Divison: "+divison)
